b33549ed370cde32d0fd0b3ea193bf1dec3e68d1
Bálint Babics <balintbabics@users.noreply.github.com>
2016-12-06 16:31:45 +0100

    initial commit

---
+++ randomBackgroundApp/Utils.swift
@@ -0,0 +1,19 @@
+//
+//  Utils.swift
+//  randomBackgroundApp
+//
+//  Created by Balint Babics on 05/12/16.
+//  Copyright © 2016 Balint Babics. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+    return UIColor(
+        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+        alpha: CGFloat(1.0)
+    )
+}
---
+++ randomBackgroundApp/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  randomBackgroundApp
+//
+//  Created by Balint Babics on 15/11/16.
+//  Copyright © 2016 Balint Babics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBOutlet weak var backgroundView: UIView!
+
+    @IBAction func getRandomColor(_ sender: Any) {
+        
+        // backgroundView.backgroundColor = randomColor();
+        backgroundView.backgroundColor = UIColor.MaterialColors.Blue;
+    }
+}
+
+func randomColor() -> UIColor {
+    
+    let hue : CGFloat = CGFloat(arc4random() % 256) / 256
+    let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.25
+    let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.25
+    return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+}
